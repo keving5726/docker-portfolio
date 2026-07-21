e53789fe60cb8e7205526a6fdd14bfeed2ed013c
Kevin González <keving5726@gmail.com>
2026-07-20 21:46:42 -0400

    Update setup-redis.sh script

--- wordpress-stack/scripts/setup-redis.sh
+++ wordpress-stack/scripts/setup-redis.sh
@@ -2,31 +2,24 @@
 #: Title:	setup-redis.sh
 #: Date:	2026-06-13
 #: Author:	"Kevin González" <keving5726@gmail.com>
-#: Version:	1.0
+#: Version:	1.1
 #: Description:	Install and activate the Redis Object Cache plugin
 #: Options:	None
 
 # Exit immediately if a command returns a non-zero status
 set -e
 
-# Wait until WordPress is fully installed and database is ready
-until wp core is-installed --allow-root
-do
-	printf "%s\n" "Waiting for WordPress installation to complete"
-	sleep 5
-done
-
 # Install and activate Redis Object Cache plugin
-if wp plugin is-installed redis-cache --allow-root
+if wp plugin is-installed redis-cache
 then
 	printf "%s\n" "Ensuring Redis Object Cache plugin is active"
-	wp plugin activate redis-cache --allow-root
+	wp plugin activate redis-cache
 else
 	printf "%s\n" "Installing and activating Redis Object Cache plugin"
-	wp plugin install redis-cache --activate --allow-root
+	wp plugin install redis-cache --activate
 fi
 
 printf "%s\n" "Enable the object cache drop-in"
-wp redis enable --allow-root
+wp redis enable
 
 printf "%s\n" "The Redis Object Cache plugin has been installed successfully"
